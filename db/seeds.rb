fa5678a683e46919cd6a6e771fa5fb8da1401faa
Trinh Tran <trantrinh.1102@gmail.com>
2016-08-15 14:23:22 +0700

    Add follow user

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ User.create!(name:  "admin",
              password_confirmation: "password",
              admin: true)
 
-99.times do |n|
+9.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -15,8 +15,16 @@ User.create!(name:  "admin",
 end
 
 users = User.order(:created_at).take(6)
-50.times do
+5.times do
   content = Faker::Lorem.sentence(5)
   title = Faker::Lorem.sentence(1)
   users.each { |user| user.entries.create!(title: title, content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..5]
+followers = users[3..4]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
